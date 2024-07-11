d76be2ee871a34d412092cca73f147e6429a8094
KayoJules <ChaoticPhoenix777@gmail.com>
2024-07-10 23:13:42 -0500

    Base classes definition

--- src/main/java/com/revature/Account/Account.java
+++ src/main/java/com/revature/Account/Account.java
@@ -1,4 +1,27 @@
 package com.revature.Account;
 
+import java.math.BigDecimal;
+
 public class Account {
+
+    private int accountId;
+    private int userId;
+    private BigDecimal balance;
+    private AccountType accountType;
+
+    public enum AccountType {
+        CHECKING,
+        SAVINGS
+    }
+
+    public int getAccountId() { return accountId; }
+    public int getUserId() { return userId; }
+    public BigDecimal getBalance() { return balance; }
+    public AccountType getAccountType() { return accountType; }
+
+    public void setAccountId(int accountId) { this.accountId = accountId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public void setBalance(BigDecimal balance) { this.balance = balance; }
+    public void setAccountType(AccountType accountType) { this.accountType = accountType; }
+
 }
